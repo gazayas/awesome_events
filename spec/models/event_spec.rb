be77b242080497d43112f2ca5d89e9241e487f7e
Gabriel Zayas <g-zayas@hotmail.com>
2016-10-18 11:09:23 +0900

    266-267ページのテスト

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -13,6 +13,22 @@ end
 
 describe Event do
 
+  describe '#rails?' do
+    context '#nameが"Rails勉強会"のとき' do
+      it 'trueを返すこと' do
+        event = create(:event, name: 'Rails勉強会')
+        expect(event.rails?).to eq true
+      end
+    end
+
+    context '#nameが"Ruby勉強会"のとき' do
+      it 'falseを返すこと' do
+        event = create(:event, name: 'Ruby勉強会')
+        expect(event.rails?).to eq false
+      end
+    end
+  end
+
   describe '#name' do
 
     context 'nilのとき' do
